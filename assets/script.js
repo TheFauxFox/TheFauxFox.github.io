3e9900da624cfb5c60d2fa0dcfaff8f667b5f98f
Faux <37963757+TheFauxFox@users.noreply.github.com>
2022-11-16 22:09:06 -0800

    A lot of updating

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+  Object.values(document.getElementsByClassName("checkbox")).forEach((el) => {
+    el.addEventListener("click", (evt) => {
+      targetClasses = evt.target.classList;
+      if (targetClasses.contains("checked")) {
+        window[evt.target.attributes.name.value] = false;
+        targetClasses.remove("checked");
+        targetClasses.add("unchecked");
+      } else {
+        window[evt.target.attributes.name.value] = true;
+        targetClasses.remove("unchecked");
+        targetClasses.add("checked");
+      }
+    });
+    window[el.attributes.name.value] = el.classList.contains("checked");
+  });
+};
